2fb99a048f4b896b2e180be8064994168212f7e5
Grimo0 <gregoir.andre@gmail.com>
2021-07-11 16:51:02 +0200

    Update Ball art name

--- src/en/Ball.hx
+++ src/en/Ball.hx
@@ -4,7 +4,7 @@ class Ball extends Entity {
 	public function new() {
 		super();
 
-		spr.set(Assets.entities, 'ballSpr');
+		spr.set(Assets.entities, 'BallSpr');
 		spr.color.set(0, .64, 1);
 
 		wid = spr.tile.width;
